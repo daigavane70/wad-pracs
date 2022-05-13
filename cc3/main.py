e2eb56976745fe1a3787197fa208a2e7ec27e9ad
IMATMI-Vedant <vedant@imatmi.com>
2022-05-14 02:15:07 +0530

    added few changes

---
+++ cc3/main.py
@@ -0,0 +1,13 @@
+def name():
+    table = {"name":"Chirag", "dept":"IT", "roll":"33179"}
+    for i in table:
+        print(i)
+        print(": ")
+        print(table[i])
+        print("<br>")
+        i = 1  
+i=1        
+while i<6:
+    name()
+    print("<br>")
+    i+=1
